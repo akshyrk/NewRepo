22438c0172c9c6b8ec142e20daaf87ced25a09ea
akshyrk <akshy77@gmail.com>
2016-09-18 19:50:18 +0530

    remote changes

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		System.out.println("Modification from the local GUI")
+		System.out.println("Modification from the local GUI"):
+		System.out.println("Changes from remote"):
 	}
 }
